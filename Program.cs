1510124983cf4ac5b0e3cc7242be8e23932c4afe
Renan Porcidonio Xavier <porcidoniorenan@gmail.com>
2025-10-20 22:33:52 -0300

    auto-create database on startup

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data S
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+	db.Database.Migrate();
+
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
--- dockerfile
+++ dockerfile
@@ -2,7 +2,7 @@
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /app
 
-# Copy everything and build
+# Copy everything and build the application
 COPY . .
 RUN dotnet publish -c Release -o out
 
